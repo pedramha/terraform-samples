44b49b4c108369e7febcc1c7465d05e9c2496459
pedram.hamidehkhan <pedram.hamidehkhan@hashicorp.com>
2022-09-15 16:55:10 +0200

    changed s3 config

--- ex2/frontendws/main.tf
+++ ex2/frontendws/main.tf
@@ -14,9 +14,9 @@ resource "aws_s3_bucket" "bucket" {
   bucket = random_pet.lambda_bucket_name.id
   acl    = "public-read"
 
-  website {
-    index_document = "index.html"
-  }
+  # website {
+  #   index_document = "index.html"
+  # }
   
 }
 
@@ -27,3 +27,14 @@ resource "aws_s3_bucket_object" "file_upload" {
     source = "${path.module}/static/${each.value}"
     etag = filemd5("${path.module}/static/${each.value}") 
 }
+
+resource "aws_s3_bucket_website_configuration" "webconfig" {
+  bucket = aws_s3_bucket.bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  }
+}
+
