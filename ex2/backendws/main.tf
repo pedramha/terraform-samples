92e026b59f61f32d7b0a94aa7bcb76bf0123ca88
pedram.hamidehkhan <pedram.hamidehkhan@hashicorp.com>
2022-09-16 12:22:13 +0200

    test lambda

--- ex2/backendws/main.tf
+++ ex2/backendws/main.tf
@@ -2,18 +2,28 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+# use a remote state
+data "terraform_remote_state" "remote-state" {
+  backend = "remote"
+  config = {
+    organization = "pedram-company"
 
+    workspaces = {
+      name = "mongodbtest"
+    }
+  }
+}
 
 module "lambda" {
   source  = "app.terraform.io/pedram-company/lambda/aws"
-  version = "0.0.3"
+  version = "0.0.5"
 
   src_path = "${var.lambda_src_path}"
   target_path = "${var.lambda_target_path}"
 }
 
 resource "aws_api_gateway_rest_api" "restapi" {
-  name = "api-carsub"
+  name = "sentinemnt-api"
   tags = {
     "owner" = "pedram@hashicorp.com"
     "env"   = "dev"
@@ -24,20 +34,36 @@ resource "aws_api_gateway_rest_api" "restapi" {
 resource "aws_api_gateway_resource" "apiresource" {
   rest_api_id = aws_api_gateway_rest_api.restapi.id
   parent_id   = aws_api_gateway_rest_api.restapi.root_resource_id
-  path_part   = "order-car"
+  path_part   = "resource"
 }
 //new method for the resource
-resource "aws_api_gateway_method" "apimethod" {
+resource "aws_api_gateway_method" "getapi" {
   rest_api_id   = aws_api_gateway_rest_api.restapi.id
   resource_id   = aws_api_gateway_resource.apiresource.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
+resource "aws_api_gateway_method" "putapi" {
+  rest_api_id   = aws_api_gateway_rest_api.restapi.id
+  resource_id   = aws_api_gateway_resource.apiresource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
 resource "aws_api_gateway_integration" "get_integration" {
   rest_api_id             = aws_api_gateway_rest_api.restapi.id
   resource_id             = aws_api_gateway_resource.apiresource.id
-  http_method             = aws_api_gateway_method.apimethod.http_method
+  http_method             = aws_api_gateway_method.getapi.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = module.lambda.lambda_invoke_arn
+}
+
+resource "aws_api_gateway_integration" "put_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.restapi.id
+  resource_id             = aws_api_gateway_resource.apiresource.id
+  http_method             = aws_api_gateway_method.putapi.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = module.lambda.lambda_invoke_arn
@@ -47,11 +73,12 @@ resource "aws_lambda_permission" "api_gw" {
   statement_id = "AllowExecutionFromAPIGateway"
   action       = "lambda:InvokeFunction"
 
-  function_name = module.lambda.lambda_arn
+  function_name = module.lambda.lambda_name
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_api_gateway_rest_api.restapi.execution_arn}/*/*"
 }
+
 
 resource "aws_api_gateway_deployment" "apideployment" {
   rest_api_id = aws_api_gateway_rest_api.restapi.id
@@ -69,14 +96,3 @@ resource "aws_api_gateway_stage" "restapistage" {
   }
 }
 
-# use a remote state
-data "terraform_remote_state" "remote-state" {
-  backend = "remote"
-  config = {
-    organization = "pedram-company"
-
-    workspaces = {
-      name = "mongodbtest"
-    }
-  }
-}
