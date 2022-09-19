2e338405f6a882adfaaaafbb9366f051a3d9a967
pedram.hamidehkhan <pedram.hamidehkhan@hashicorp.com>
2022-09-19 09:44:01 +0200

    2xlarge

--- ex1/main.tf
+++ ex1/main.tf
@@ -18,7 +18,8 @@ resource "aws_security_group" "instance" {
 }
 resource "aws_instance" "example" {
   ami                    = "ami-785db401"
-  instance_type          = "t2.micro"
+  # instance_type          = "t2.micro"
+  instance_type          = "t2.2xlarge"
   availability_zone      = "eu-west-1a"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
   tags = {
