644eef356b4f6f81f3ce622b529855281299071a
pedram.hamidehkhan <pedram.hamidehkhan@hashicorp.com>
2022-09-19 08:47:18 +0200

    amend policy

--- ex2/frontendws/main.tf
+++ ex2/frontendws/main.tf
@@ -12,9 +12,6 @@ resource "random_pet" "lambda_bucket_name" {
 # s3
 resource "aws_s3_bucket" "bucket" {
   bucket = random_pet.lambda_bucket_name.id
-  acl    = "public-read"
-  policy = file("policy.json")
-
   website {
     index_document = "index.html"
   }
@@ -22,9 +19,37 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 resource "aws_s3_bucket_object" "file_upload" {
+  content_type = "text/html"
   for_each = fileset("${path.module}/static", "**")
     bucket = aws_s3_bucket.bucket.id
     key = each.value
     source = "${path.module}/static/${each.value}"
     etag = filemd5("${path.module}/static/${each.value}") 
 }
+
+resource "aws_s3_bucket_acl" "site" {
+  bucket = aws_s3_bucket.bucket.id
+
+  acl = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "site" {
+  bucket = aws_s3_bucket.bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject",
+        Resource = [
+          aws_s3_bucket.bucket.arn,
+          "${aws_s3_bucket.bucket.arn}/*",
+        ]
+      },
+    ]
+  })
+}
+
