d70a565afcac5e8086f5a5e7bfa0cd011ce55005
pedram.hamidehkhan <pedram.hamidehkhan@hashicorp.com>
2023-03-21 14:24:10 +0100

    amended config

--- ex1/output.tf
+++ ex1/output.tf
@@ -3,6 +3,10 @@
 output "public_ip" {
   value = "http://${aws_instance.example.public_ip}:8080"
 }
+output "hypothetical-connectionstring" {
+  value = "http://${aws_instance.example.public_ip}"
+  sensitive = true
+}
 # output "keypair"{
 #   value = tls_private_key.keypair.private_key_pem
 #   sensitive = true
